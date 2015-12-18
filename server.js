3f356359cf5b990db3c7b455d4386c0cfd7c575a
dispxxx <contact@dispix.net>
2015-12-18 16:58:43 +0100

    Update

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-var express 	= require('express')
-var session 	= require('express-session')
-var cookieParser = require('cookie-parser')
-var mysql 		= require('mysql')
-var app 		= express()
-var jade 		= require('jade')
 
-// var db 	= mysql.createConnection({
-// 				host : 'localhost',
-// 				user : 'pma',
-// 				password : '',
-// 				database : 'test'})
-//
-// db.connect(function(err) {
-// 	if (err) {
-// 		console.error('error connecting: ' + err.stack);
-// 		return;
+// Modules
+var express 		= require('express')
+var session 		= require('express-session')
+var cookieParser 	= require('cookie-parser')
+var mysql 			= require('mysql')
+var app 			= express()
+var jade 			= require('jade')
 
+
+// Mysql connection
+var sql = require('./sql.js')
+var db 	= mysql.createConnection({
+				host : sql.host,
+				user : sql.user,
+				password : sql.password,
+				database : sql.database
+})
+
+db.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack)
+		return
+	}
+})
+
+
+// Default settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
@@ -23,34 +33,24 @@ app.use(cookieParser())
 app.use(session({secret:'bla123bla'}))
 app.use(express.static('public'))
 
-// Public routes
-app.get('/login', function(req, res)
-{
-	if (req.session.sessionId)
-	{
-		res.location('/home')
-	}
-	else
-	{
-		res.render('skel.jade', {page: 'content/login'})
-	}
-})
-app.get('/register', function(req, res)
-{
-	if (req.session.sessionId)
-	{
-		res.location('/home')
-	}
-	else
-	{
-		res.render('skel.jade', {locals: {page: 'content/register'}})
-	}
-})
 
-// Default route
-app.get('/', function(req, res)
+// Pages
+var pages 	=
 {
-	res.render('skel.jade')
+	'user'		: new (require('./controllers/user.controller.js'))(db),
+	'article'	: new (require('./controllers/article.controller.js'))(db),
+	//'home'		: new (require('./controllers/default.controller.js'))(db)
+}
+
+
+// Routes
+app.get('/:page/:action', function(req, res)
+{
+	var page 	= req.params.page
+	if (pages[page] !== undefined)
+	{
+		pages[page].exec(req, res)
+	}
 })
 
 if (app.listen(8080))
